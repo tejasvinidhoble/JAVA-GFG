cc50597968b4557563f25b500eae0f10c74c7281
Tejasvini Dhoble <124334652+tejasvinidhoble@users.noreply.github.com>
2026-01-09 00:04:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarray with k odds/count-subarray-with-k-odds.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countSubarrays(int[] arr, int k) {
+        // code here
+        return atMostK(arr, k) - atMostK(arr, k - 1);
+    }
+
+    private int atMostK(int[] arr, int k) {
+        int left = 0, count = 0, oddCount = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] % 2 == 1) {
+                oddCount++;
+            }
+
+            while (oddCount > k) {
+                if (arr[left] % 2 == 1) {
+                    oddCount--;
+                }
+                left++;
+            }
+
+            count += (right - left + 1);
+        }
+
+        return count; 
+    }
+}
